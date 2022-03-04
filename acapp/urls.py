fe1b666687dac7c495b37443134218b46c11bdcd
zinkt_django <zinkt@foxmail.com>
2022-03-04 15:33:58 +0800

    show img menu

--- acapp/urls.py
+++ acapp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #ath('game/', include('game.urls')),
+    path('', include('game.urls.index')),
 ]
